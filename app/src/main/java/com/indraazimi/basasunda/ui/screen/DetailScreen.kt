06b6e8b5795f5faec117795d9ebe053c3a3b8bc4
Muh Daffah Putra M <muh.daffah27@gmail.com>
2025-05-21 04:08:30 +0700

    Membuat halaman detail

---
+++ app/src/main/java/com/indraazimi/basasunda/ui/screen/DetailScreen.kt
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2023-2025 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman Berbasis Web 1.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.basasunda.ui.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.indraazimi.basasunda.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(category: String, navController: NavController) {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text( text = category )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Back",
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .clickable { navController.popBackStack() }
+                    )
+                }
+
+            )
+        }
+    ) {
+            innerPadding ->
+        DetailContent(
+            modifier = Modifier.padding(innerPadding),
+            category = category
+        )
+    }
+}
+
+@Composable
+fun DetailContent(modifier: Modifier = Modifier, category: String) {
+    val words = when (category) {
+        "Keluarga" -> listOf(
+            "Ibu", "Ayah", "Saudara", "Saudari", "Kakek", "Nenek", "Paman", "Bibi", "Sepupu"
+        )
+        else -> listOf("Belum ada data untuk kategori ini.")
+    }
+
+    LazyColumn (
+        modifier = modifier
+            .background(MaterialTheme.colorScheme.background),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(words.size) { index ->
+            WordItem(
+                title = words[index],
+                sunda = "Sunda",
+                onClick = { }
+            )
+            HorizontalDivider()
+        }
+    }
+}
+
+@Composable
+fun WordItem(
+    title: String,
+    sunda: String,
+    onClick: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+            .background(MaterialTheme.colorScheme.surface)
+            .height(88.dp)
+            .padding(start = 16.dp),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.bodyLarge.copy(
+                fontWeight = FontWeight.Bold
+            ),
+            color = MaterialTheme.colorScheme.onSurface,
+        )
+        Text(
+            text = sunda,
+            style = MaterialTheme.typography.bodyLarge.copy(
+                fontWeight = FontWeight.Normal
+            ),
+            color = MaterialTheme.colorScheme.onSurface,
+        )
+    }
+}
+
