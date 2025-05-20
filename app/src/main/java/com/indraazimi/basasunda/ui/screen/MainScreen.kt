9875328aa9ff9447823ee73aeeb1d56e49bceb33
Muh Daffah Putra M <muh.daffah27@gmail.com>
2025-05-21 03:41:26 +0700

    Membuat mainscreen

---
+++ app/src/main/java/com/indraazimi/basasunda/ui/screen/MainScreen.kt
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2023-2025 Indra Azimi. All rights reserved.
+ *
+ * Dibuat untuk kelas Pemrograman Berbasis Web 1.
+ * Dilarang melakukan penggandaan dan atau komersialisasi,
+ * sebagian atau seluruh bagian, baik cetak maupun elektronik
+ * terhadap project ini tanpa izin pemilik hak cipta.
+ */
+
+package com.indraazimi.basasunda.ui.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.indraazimi.basasunda.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(R.string.app_name))
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+            )
+        }
+    ) {
+        innerPadding ->
+        MainContent(
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@Composable
+fun MainContent(modifier: Modifier = Modifier) {
+    val categories = listOf(
+        "Keluarga",
+        "Hewan",
+        "Buah",
+        "Sayuran",
+        "Benda",
+        "Angka",
+        "Warna",
+        "Hari",
+        "Bulan",
+        "Cuaca"
+    )
+
+    LazyColumn (
+        modifier = modifier
+            .background(MaterialTheme.colorScheme.background),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(categories.size) { index ->
+            CategoryItem(
+                title = categories[index],
+                onClick = { }
+            )
+            HorizontalDivider()
+        }
+    }
+}
+
+@Composable
+fun CategoryItem(
+    title: String,
+    onClick: () -> Unit = {}
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+            .background(MaterialTheme.colorScheme.surface)
+            .height(88.dp)
+            .padding(start = 16.dp),
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.bodyLarge.copy(
+                fontWeight = FontWeight.Bold
+            ),
+            color = MaterialTheme.colorScheme.onSurface,
+        )
+    }
+}
+
